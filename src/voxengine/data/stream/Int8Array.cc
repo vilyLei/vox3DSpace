5144d7ba945545f860e58e3af35d39bd5e15c89d
vilyLei <vily313@126.com>
2022-08-18 22:06:14 +0800

    fix: add Int8Array.

---
+++ src/voxengine/data/stream/Int8Array.cc
@@ -0,0 +1,33 @@
+#include "ArrayBuffer.h"
+#include "Int8Array.h"
+
+namespace model
+{
+namespace base
+{
+Int8Array::Int8Array(std::shared_ptr<ArrayBuffer> arrBuf, Uint32 byteOffset, Uint32 length) :
+    DataView(arrBuf, byteOffset, length)
+{
+    buildData();
+}
+Int8Array::Int8Array(Uint32 length) :
+    DataView(length)
+{
+    buildData();
+}
+Int8Array::~Int8Array()
+{
+    //std::cout << "Int8Array::deconstructor()..." << std::endl;
+}
+void Int8Array::buildData()
+{
+    buildBytesData(1);
+}
+Int8& Int8Array::operator[](Int32 i)
+{
+    assertIndex(i);
+    return m_bytes[i];
+}
+
+} // namespace base
+} // namespace model
