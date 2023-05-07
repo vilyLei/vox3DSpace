667ee029fd7a5ed4bba7f03c416dcf81346a17b1
vilyLei <vily313@126.com>
2023-05-07 08:51:39 +0800

    fix: add some primitive geom calc code files.

---
+++ src/common/math/Ray.h
@@ -0,0 +1,78 @@
+#pragma  once 
+#include "FCConfig.h"
+#include <limits>
+#include "vec3.h"
+#include "mathutil.h"
+
+
+namespace baseMath {
+
+	class Box;
+	class Sphere;
+	class Plane;
+
+	/** Representation of a ray in space, i.e. a line with an origin and direction. */
+	class BaseExport Ray
+	{
+	protected:
+		Float3 mOrigin;
+		Float3 mDirection;
+	public:
+		Ray() :mOrigin(Float3::ZERO), mDirection(Float3::UNIT_Z) {}
+		Ray(const Float3& origin, const Float3& direction)
+			:mOrigin(origin), mDirection(direction) {}
+
+		/** Sets the origin of the ray. */
+		void setOrigin(const Float3& origin) { mOrigin = origin; }
+		/** Gets the origin of the ray. */
+		const Float3& getOrigin(void) const { return mOrigin; }
+
+		/** Sets the direction of the ray. */
+		void setDirection(const Float3& dir) { mDirection = dir; }
+		/** Gets the direction of the ray. */
+		const Float3& getDirection(void) const { return mDirection; }
+
+		/** Gets the position of a point t units along the ray. */
+		Float3 getPoint(float t) const {
+			return Float3(mOrigin + (mDirection * t));
+		}
+
+		/** Gets the position of a point t units along the ray. */
+		Float3 operator*(float t) const {
+			return getPoint(t);
+		}
+
+		/** Tests whether this ray intersects the given plane.
+		@return A pair structure where the first element indicates whether
+			an intersection occurs, and if true, the second element will
+			indicate the distance along the ray at which it intersects.
+			This can be converted to a point in space by calling getPoint().
+		*/
+		std::pair<bool, float> intersects(const Plane& p) const
+		{
+			return Math::intersects(*this, p);
+		}
+
+		/** Tests whether this ray intersects the given sphere.
+		@return A pair structure where the first element indicates whether
+			an intersection occurs, and if true, the second element will
+			indicate the distance along the ray at which it intersects.
+			This can be converted to a point in space by calling getPoint().
+		*/
+		std::pair<bool, float> intersects(const Sphere& s) const
+		{
+			return Math::intersects(*this, s);
+		}
+		/** Tests whether this ray intersects the given box.
+		@return A pair structure where the first element indicates whether
+			an intersection occurs, and if true, the second element will
+			indicate the distance along the ray at which it intersects.
+			This can be converted to a point in space by calling getPoint().
+		*/
+		std::pair<bool, float> intersects(const Box& box) const
+		{
+			return Math::intersects(*this, box);
+		}
+
+	};
+}
---
+++ src/common/math/Sphere.h
@@ -0,0 +1,92 @@
+#pragma once
+#include "FCConfig.h"
+#include "vec3.h"
+#include "mathutil.h"
+
+namespace baseMath
+{
+	class Box;
+	class Plane;
+	/** A sphere primitive, mostly used for bounds checking.
+	@remarks
+		A sphere in math texts is normally represented by the function
+		x^2 + y^2 + z^2 = r^2 (for sphere's centered on the origin). Cap stores spheres
+		simply as a center point and a radius.
+	*/
+	class BaseExport Sphere
+	{
+	protected:
+		float mRadius;
+		Float3 mCenter;
+	public:
+		/** Standard constructor - creates a unit sphere around the origin.*/
+		Sphere() : mRadius(1.0), mCenter(Float3::ZERO) {}
+		/** Constructor allowing arbitrary spheres.
+			@param center The center point of the sphere.
+			@param radius The radius of the sphere.
+		*/
+		Sphere(const Float3& center, float radius)
+			: mRadius(radius), mCenter(center) {}
+
+		/** Returns the radius of the sphere. */
+		float getRadius(void) const { return mRadius; }
+
+		/** Sets the radius of the sphere. */
+		void setRadius(float radius) { mRadius = radius; }
+
+		/** Returns the center point of the sphere. */
+		const Float3& getCenter(void) const { return mCenter; }
+
+		/** Sets the center point of the sphere. */
+		void setCenter(const Float3& center) { mCenter = center; }
+
+		/** Returns whether or not this sphere intersects another sphere. */
+		bool intersects(const Sphere& s) const
+		{
+			return (s.mCenter - mCenter).squaredLength() <=
+				Math::Sqr(s.mRadius + mRadius);
+		}
+		/** Returns whether or not this sphere intersects a box. */
+		bool intersects(const Box& box) const
+		{
+			return Math::intersects(*this, box);
+		}
+		/** Returns whether or not this sphere intersects a plane. */
+		bool intersects(const Plane& plane) const
+		{
+			return Math::intersects(*this, plane);
+		}
+		/** Returns whether or not this sphere intersects a point. */
+		bool intersects(const Float3& v) const
+		{
+			return ((v - mCenter).squaredLength() <= Math::Sqr(mRadius));
+		}
+		/** Merges another Sphere into the current sphere */
+		void merge(const Sphere& oth)
+		{
+			Float3 diff = oth.getCenter() - mCenter;
+			float lengthSq = diff.squaredLength();
+			float radiusDiff = oth.getRadius() - mRadius;
+
+			// Early-out
+			if (Math::Sqr(radiusDiff) >= lengthSq)
+			{
+				// One fully contains the other
+				if (radiusDiff <= 0.0f)
+					return; // no change
+				else
+				{
+					mCenter = oth.getCenter();
+					mRadius = oth.getRadius();
+					return;
+				}
+			}
+
+			float length = Math::Sqrt(lengthSq);
+			float t = (length + radiusDiff) / (2.0f * length);
+			mCenter = mCenter + diff * t;
+			mRadius = 0.5f * (length + mRadius + oth.getRadius());
+		}
+
+	};
+}
