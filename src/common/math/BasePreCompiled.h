783050fec9166617728a6487a831b73733fb5a06
vilyLei <vily313@126.com>
2023-05-07 08:48:06 +0800

    fix: add some precompile code files.

---
+++ src/common/math/BasePreCompiled.h
@@ -0,0 +1,43 @@
+#ifndef BASE_PRECOMPILED_H
+#define BASE_PRECOMPILED_H
+
+#include "FCConfig.h"
+
+// standard
+#include <fcntl.h>
+#include <cstdio>
+#include <cassert>
+#include <time.h>
+#include <cfloat>
+#ifdef FC_OS_WIN32
+#define _USE_MATH_DEFINES
+#endif // FC_OS_WIN32
+#include <cmath>
+#include <climits>
+#include <cstring>
+#ifdef FC_OS_WIN32
+#include <direct.h>
+
+#ifndef 	WIN32_LEAN_AND_MEAN
+#	define WIN32_LEAN_AND_MEAN
+#ifndef 	NOMINMAX
+#	define NOMINMAX
+#endif 
+#endif 
+
+#include <windows.h>
+#include <crtdbg.h>
+#include <shellapi.h>
+#include <Rpc.h>
+#endif
+
+#include "BaseType.h"
+
+// 2D rendering macros
+//#ifndef    RHI_GENERATE_2D_COMMAND
+//#	define RHI_GENERATE_2D_COMMAND
+//#endif
+
+
+#endif // BASE_PRECOMPILED_H
+
