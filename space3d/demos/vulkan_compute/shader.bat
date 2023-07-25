2047e365baac9bdc60d8ae03dc6ff605c2a9f0de
vilyLei <vily313@126.com>
2023-07-25 21:27:01 +0800

    fix: add a vulkan compute shader demo.

---
+++ space3d/demos/vulkan_compute/shader.bat
@@ -0,0 +1,4 @@
+c:/VulkanSDK/1.3.231.1/Bin/glslc.exe test.vert -o test_vert.spv
+c:/VulkanSDK/1.3.231.1/Bin/glslc.exe test.frag -o test_frag.spv
+c:/VulkanSDK/1.3.231.1/Bin/glslc.exe test.comp -o test_comp.spv
+pause
