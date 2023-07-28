b14033f1eabb75f78da475b6557a9f25064fafb5
vilyLei <vily313@126.com>
2023-07-28 22:11:06 +0800

    fix: add the opengl demo building cmake file.

---
+++ space3d/cmake/vox3dspace_executable_oglDemos.cmake
@@ -0,0 +1,37 @@
+macro(build_executable_oglDemos)  
+
+  set(test_oglDemos_folder_name "OpenglDemos")
+  set(oglDemo_dir "${vox3dspace_root}/testCases/openglDemos/")
+
+  function(vox3dspace_add_oglDemo_executableByName DEMO_NAME DEMO_SOURCES)
+
+    # message(STATUS ">>> >>> DEMO_NAME: ${DEMO_NAME}")
+    # message(STATUS ">>> >>> DEMO_SOURCES: ${DEMO_SOURCES}")
+
+    set(demoDir "${oglDemo_dir}/${DEMO_NAME}/")
+    vox3dspace_add_executable(NAME
+      ${DEMO_NAME}
+      SOURCES
+      "${demoDir}/shader.cc"
+      "${demoDir}/shader.h"
+      "${demoDir}/${DEMO_NAME}.cc"
+      ${DEMO_SOURCES}
+      FOLDER
+      ${test_oglDemos_folder_name}
+      DEFINES
+      ${vox3dspace_defines}
+      RPOJECT_FLAGS
+      ${VOX3DSPACE_CXX_FLAGS}
+      INCLUDES
+      ${vox3dspace_opengl_libs_include_paths}
+      "${vox3dspace_root}/libsInclude"
+      LIB_INCLUDES
+      ${vox3dspace_opengl_libs_paths}
+      "${vox3dspace_libs_root}/stb/lib"
+      LIB_DEPS
+      ${vox3dspace_opengl_dependency})
+    
+  endfunction()
+
+  vox3dspace_add_oglDemo_executableByName("textures" "")
+endmacro()
