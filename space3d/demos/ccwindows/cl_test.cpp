2251ae25581574d065f59e8e14639f7c87f6bb68
vilyLei <vily313@126.com>
2023-08-01 23:53:00 +0800

    fix: append some comments.

--- space3d/demos/ccwindows/cl_test.cpp
+++ space3d/demos/ccwindows/cl_test.cpp
@@ -6,4 +6,9 @@ int main()
     std::cout << "main exec end ...\n";
     return EXIT_SUCCESS;
 }
+// https://learn.microsoft.com/en-us/cpp/build/reference/compiler-options?view=msvc-170
+// https://learn.microsoft.com/en-us/cpp/build/reference/compiler-command-line-syntax?view=msvc-170
+// https://learn.microsoft.com/en-us/cpp/build/reference/compiler-options-listed-alphabetically?view=msvc-170
+// https://learn.microsoft.com/en-us/cpp/build/reference/eh-exception-handling-model?view=msvc-170
+
 // compile command: cl /EHsc cl_test.cpp
