1c6494a5ceac712fc2e37d82cb8a0e343e0ae4cb
vilyLei <vily313@126.com>
2023-09-05 19:12:04 +0800

    fix: adjust a few codes.

--- space3d/demos/ccplus/parallel/threadPool.h
+++ space3d/demos/ccplus/parallel/threadPool.h
@@ -9,11 +9,11 @@
 #include <stdexcept>
 #include <functional>
 #include <compare>
+#include <future>
+#include <type_traits>
 using namespace std::literals;
 
 namespace parallel::threadPool
-{
-namespace threadPool_01
 {
 template <typename T>
 class thread_safe_queue
@@ -91,6 +91,106 @@ public:
         }
     }
 };
+namespace threadPool_02
+{
+class function_wrapper
+{
+    struct impl_base
+    {
+        virtual void call() = 0;
+        virtual ~impl_base() {}
+    };
+    std::unique_ptr<impl_base> impl;
+
+    template <typename F>
+    struct impl_type : impl_base
+    {
+        F f;
+        impl_type(F&& f_) :
+            f(std::move(f_)) {}
+        void call() { f(); }
+    };
+
+public:
+    template <typename F>
+    function_wrapper(F&& f) :
+        impl(new impl_type<F>(std::move(f)))
+    {}
+    void operator()() { impl->call(); }
+    function_wrapper() = default;
+    function_wrapper(function_wrapper&& other) :
+        impl(std::move(other.impl))
+    {}
+    function_wrapper& operator=(function_wrapper&& other)
+    {
+        impl = std::move(other.impl);
+        return *this;
+    }
+    function_wrapper(const function_wrapper&)            = delete;
+    function_wrapper(function_wrapper&)                  = delete;
+    function_wrapper& operator=(const function_wrapper&) = delete;
+};
+class thread_pool
+{
+    std::atomic_bool                    done;
+
+    thread_safe_queue<function_wrapper>          work_queue; // 使用 function_wrapper，而非使用 std::function
+    void worker_thread()
+    {
+        while (!done)
+        {
+            //function_wrapper task;
+            //if (work_queue.try_pop(task))
+            //{
+            //    task();
+            //}
+            //else
+            //{
+            //    std::this_thread::yield();
+            //}
+            auto task = work_queue.try_pop();
+            if (task) // 5
+            {
+                (*task)(); // 6
+            }
+            else
+            {
+                std::this_thread::yield(); // 7
+            }
+        }
+    }
+
+public:
+
+#if __cplusplus >= 202002L
+    template <typename FunctionType>
+    std::future<typename std::invoke_result<FunctionType()>::type>
+    submit(FunctionType f)
+    {
+        typedef typename std::invoke_result<FunctionType()>::type result_type;
+        std::packaged_task<result_type()> task(std::move(f));
+        std::future<result_type>          res(task.get_future());
+        work_queue.push(std::move(task));
+        return res;
+    }
+#else
+    template <typename FunctionType>
+    std::future<typename std::result_of<FunctionType()>::type>
+    submit(FunctionType f)
+    {
+        typedef typename std::result_of<FunctionType()>::type
+                                          result_type;
+        std::packaged_task<result_type()> task(std::move(f));
+        std::future<result_type>          res(task.get_future());
+        work_queue.push(std::move(task));
+        return res;
+    }
+#endif
+    // rest as before
+};
+}
+namespace threadPool_01
+{
 ///*
 
 uint64_t getThisThreadIdWithInt()
@@ -107,7 +207,7 @@ std::string getThisThreadIdWithString()
     return ss.str();
 }
 class thread_pool
-    {
+{
     std::atomic_bool                         done;
     thread_safe_queue<std::function<void()>> work_queue; // 1
     std::vector<std::thread>                 threads;    // 2
