5f59135144a6f28dc2c7e826cd1e078d1ee56e4b
vilyLei <vily313@126.com>
2025-08-10 12:04:29 +0800

    chore: improve some test codes.

--- voxol/src/Voxol/Motion/RenderCmdWorld.h
+++ voxol/src/Voxol/Motion/RenderCmdWorld.h
@@ -9,23 +9,69 @@
 
 namespace Voxol::Motion
 {
+struct Point2DDesc
+{
+    float x{0};
+    float y{0};
+
+    bool isEqual(const Point2DDesc& pos) const;
+};
+struct SizeDesc
+{
+    float width{0};
+    float height{0};
+    bool  isEqual(const SizeDesc& desc) const;
+};
+struct ViewDesc
+{
+    Point2DDesc position{};
+    float       zoom{1};
+};
+
+struct ViewComponent
+{
+    ViewDesc desc{};
+    Point2DDesc mousePos{};
+    float    viewScale{1};
+
+    bool     viewZoomWithFixPos(const Point2DDesc& zoomPos, float dstScale);
+    bool     updateViewZoom(const Point2DDesc& fixPos, float delta, float speed = 1.2f);
+
+    void moveBegin(const Point2DDesc& pos);
+    bool move(const Point2DDesc& pos);
+    void moveEnd(const Point2DDesc& pos);
+private:
+    Point2DDesc fixOriginPos{};
+    Point2DDesc fixPos{};
+    Point2DDesc fixViewPos{};
+    bool moving{false};
+};
+
+struct CanvasDesc
+{
+    SizeDesc      size{};
+    ViewComponent view{};
+    int           index{0};
+};
 
 class RenderCmdWorld
 {
 public:
     std::vector<uint32_t>        commands{};
     std::vector<DrawCmdTestNode> cmdNodes{};
-    Voxol::Math::Mat33 projMat{};
+    Voxol::Math::Mat33           viewMat{};
+    Voxol::Math::Mat33           projMat{};
+
+    Point2DDesc mousePos{};
+    CanvasDesc  canvas{};
+    ViewDesc    view{};
 
     std::vector<uint8_t> buffer{};
 
     bool dirty = true;
 
-    int ctxWidth = 512;
-    int ctxHeight = 512;
-
 public:
-    RenderCmdWorld(/* args */);
+    RenderCmdWorld();
     ~RenderCmdWorld();
 
 public:
@@ -33,6 +79,7 @@ public:
     void           run();
     void           setGPUCtxSize(int w, int h);
     void           setMouseXY(float x, float y);
+    void           setMouseParams(float x, float y, int type, int flag);
     const uint8_t* cmdBuffer() const;
 
 private:
@@ -41,5 +88,5 @@ private:
     uint32_t mTailData[2]{0x0, 0x0};
 };
 
-} // namespace Voxol::Render
+} // namespace Voxol::Motion
 #endif
