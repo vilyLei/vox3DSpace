615cbd5c020d0eb796306b1e70eb441c74e62e5e
vilyLei <vily313@126.com>
2025-08-14 22:24:16 +0800

    chore: adjus shader codes.

---
+++ voxol/src/JSCode/ShaderCodes.js
@@ -0,0 +1,66 @@
+"use strict";
+
+export const vertSourceScreenV3 = `#version 300 es
+precision highp float;
+
+layout(location = 0) in vec2 a_pos;
+uniform mat3 u_objMat;
+void main() {
+    vec3 pos = u_objMat * vec3(a_pos.xy, 1.0);
+    gl_Position = vec4(pos.xy, 0.0, 1.0);
+}
+`;
+export const vertSourceMVPV3 = `#version 300 es
+precision highp float;
+
+layout(location = 0) in vec2 a_pos;
+uniform mat3 u_objMat;
+uniform mat3 u_viewMat;
+uniform mat3 u_projMat;
+void main() {
+    mat3 trans = u_projMat * u_viewMat * u_objMat;
+    vec3 pos = trans * vec3(a_pos.xy, 1.0);
+    gl_Position = vec4(pos.xy, 0.0, 1.0);
+}
+`;
+
+export const fragSource = `#version 300 es
+precision mediump float;
+uniform vec4 u_color;
+out vec4 outColor;
+void main() {
+    outColor = u_color;
+}
+`;
+
+export function getVertSourceV3SegN(n) {
+
+    const vertSourceV3SegN = `#version 300 es
+precision highp float;
+
+layout(location = 0) in vec3 a_pos;
+uniform mat3 u_transforms[${n}];
+out float v_idx;
+void main() {
+    v_idx = a_pos.z;
+    int idx = int(a_pos.z);
+    mat3 trans = u_transforms[idx];
+    vec3 pos = trans * vec3(a_pos.xy, 1.0);
+    gl_Position = vec4(pos.xy, 0.0, 1.0);
+}
+`;
+    return vertSourceV3SegN;
+}
+
+export function getFragSourceSegN(n) {
+    const fragSourceSegN = `#version 300 es
+precision mediump float;
+uniform vec4 u_colors[${n}];
+in float v_idx;
+out vec4 outColor;
+void main() {
+    outColor = u_colors[int(v_idx)];
+}
+`;
+    return fragSourceSegN;
+}
--- voxol/src/JSCode/SimpleCacheRenderer.js
+++ voxol/src/JSCode/SimpleCacheRenderer.js
@@ -3,71 +3,8 @@
 import { ShaderBuilder } from './ShaderModule.js';
 import { VertexBuilder } from './VertexModule.js';
 import { BatchROUnit, MVPROUnit, ROUnit } from './ROUnitModule.js';
+import { vertSourceMVPV3, vertSourceScreenV3, fragSource, getVertSourceV3SegN, getFragSourceSegN} from './ShaderCodes.js';
 
-const vertSourceScreenV3 = `#version 300 es
-precision highp float;
-
-layout(location = 0) in vec2 a_pos;
-uniform mat3 u_objMat;
-void main() {
-    vec3 pos = u_objMat * vec3(a_pos.xy, 1.0);
-    gl_Position = vec4(pos.xy, 0.0, 1.0);
-}
-`;
-const vertSourceMVPV3 = `#version 300 es
-precision highp float;
-
-layout(location = 0) in vec2 a_pos;
-uniform mat3 u_objMat;
-uniform mat3 u_viewMat;
-uniform mat3 u_projMat;
-void main() {
-    mat3 trans = u_projMat * u_viewMat * u_objMat;
-    vec3 pos = trans * vec3(a_pos.xy, 1.0);
-    gl_Position = vec4(pos.xy, 0.0, 1.0);
-}
-`;
-
-const fragSource = `#version 300 es
-precision mediump float;
-uniform vec4 u_color;
-out vec4 outColor;
-void main() {
-    outColor = u_color;
-}
-`;
-
-function getVertSourceV3SegN(n) {
-
-    const vertSourceV3SegN = `#version 300 es
-precision highp float;
-
-layout(location = 0) in vec3 a_pos;
-uniform mat3 u_transforms[${n}];
-out float v_idx;
-void main() {
-    v_idx = a_pos.z;
-    int idx = int(a_pos.z);
-    mat3 trans = u_transforms[idx];
-    vec3 pos = trans * vec3(a_pos.xy, 1.0);
-    gl_Position = vec4(pos.xy, 0.0, 1.0);
-}
-`;
-    return vertSourceV3SegN;
-}
-
-function getFragSourceSegN(n) {
-    const fragSourceSegN = `#version 300 es
-precision mediump float;
-uniform vec4 u_colors[${n}];
-in float v_idx;
-out vec4 outColor;
-void main() {
-    outColor = u_colors[int(v_idx)];
-}
-`;
-    return fragSourceSegN;
-}
 
 export function printWith9Number(numArr, index) {
 
@@ -188,7 +125,6 @@ export class SimpleCacheDrawer {
         let viewTransDesc = this.moduleIns.viewTransDesc;
         this.mvpUnit.viewMatData = viewTransDesc.viewF32;
         this.mvpUnit.projMatData = viewTransDesc.projF32;
-        // mvpUnit
 
     }
 
@@ -236,7 +172,6 @@ export class SimpleCacheDrawer {
         let cmdIndex = batchEle.getElementDataIndex();
         let dataU32 = batchEle.heapU32;
         let dataF32 = batchEle.heapF32;
-
 
         this.batchUnit.bind(gl);
 
