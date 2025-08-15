4a656ff2f5808837bd6dfcf13a4c9c2db8130348
vilyLei <vily313@126.com>
2025-08-15 22:58:20 +0800

    chore: adjust some codes.

--- voxol/src/JSCode/TextureModule.js
+++ voxol/src/JSCode/TextureModule.js
@@ -3,18 +3,6 @@ export class TextureBuilder {
 
     static createTextureFromImage(gl, image) {
 
-        // const tex = gl.createTexture();
-        // gl.bindTexture(gl.TEXTURE_2D, tex);
-        // gl.texImage2D(
-        //     gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA,
-        //     gl.UNSIGNED_BYTE, image
-        // );
-        // // settine params
-        // gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
-        // gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-        // gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
-        // gl.generateMipmap(gl.TEXTURE_2D);
-
         const tex = gl.createTexture();
         gl.bindTexture(gl.TEXTURE_2D, tex);
 
