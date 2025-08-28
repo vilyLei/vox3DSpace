1597410a0093d784aee5578ca462f1602de0676f
vilyLei <vily313@126.com>
2025-08-28 22:34:29 +0800

    chore: adjust some codes.

--- voxol/src/Voxol/Motion/RenderCmdWorld.cpp
+++ voxol/src/Voxol/Motion/RenderCmdWorld.cpp
@@ -21,86 +21,7 @@ void RenderCmdWorld::initialize()
 
     nodeScene.initialize();
     bufBuilder.initialize((nodeScene.getNodesTotal() + 8) * sizeof(DrawingCmdDesc));
-    /*
-    using namespace Voxol::Math;
 
-
-    // printf("RenderCmdWorld::initialize() viewMat:\n");
-
-    auto cmdsTotal = 6;
-
-    auto rn             = 3 * 1;
-    auto cn             = 4 * 1;
-    auto cmdsBatchTotal = rn * cn;
-    auto rsize          = 70.0f;
-
-    auto total = cmdsBatchTotal + cmdsTotal;
-    printf("Voxol::Motion::RenderCmdWorld::initialize() total: %d\n", total);
-
-    bufBuilder.initialize((total + 8) * sizeof(DrawingCmdDesc));
-
-    commands.resize(total);
-    cmdBatchNodes.resize(cmdsBatchTotal);
-    cmdNodes.resize(cmdsTotal);
-
-    auto  index = 0;
-    float dis   = 2;
-    for (auto i = 0; i < rn; ++i)
-    {
-        auto py = 50 + (i * (rsize + dis));
-        for (auto j = 0; j < cn; ++j)
-        {
-            auto px         = 50 + (j * (rsize + dis));
-            commands[index] = 1;
-            auto& node      = cmdBatchNodes[index];
-            auto& desc      = node.drcDesc;
-            desc.color      = 0xff0000aa | ((index % 256) << 16);
-            desc.color      = desc.color | (((i * j * 2) % 256) << 16);
-            // printf("node.color: %X\n", node.color);
-            desc.bounds.setXY(px, py);
-            desc.bounds.setSize(rsize, rsize);
-            desc.mroid = 1;
-
-
-            node.moveingNode.rect = desc.bounds;
-            node.init();
-
-            index++;
-        }
-    }
-
-    if (!cmdNodes.empty())
-    {
-        auto  nodeIndex      = 0;
-        auto& node0          = cmdNodes[nodeIndex];
-        node0.drcDesc.color  = 0xff00aaaa;
-        node0.drcDesc.bounds = {{300, 300}, 128, 128};
-        node0.drcDesc.mroid  = 2;
-        node0.rotation       = 0.3f;
-        testRotationOp(node0);
-
-        nodeIndex++;
-        auto& node1          = cmdNodes[nodeIndex];
-        node1.drcDesc.color  = 0xffffffff;
-        node1.drcDesc.bounds = {{500, 350}, 128, 128};
-        node1.drcDesc.mroid  = 3;
-
-        nodeIndex++;
-        auto& node2          = cmdNodes[nodeIndex];
-        node2.drcDesc.color  = 0xff003333;
-        node2.drcDesc.bounds = {{300, 530}, 128, 128};
-        node2.drcDesc.mroid  = 2;
-        nodeIndex++;
-        for (auto i = 0; i < 3; ++i)
-        {
-            auto& drcDesc  = cmdNodes[nodeIndex].drcDesc;
-            drcDesc.color  = 0xffbbbb00;
-            drcDesc.bounds = {{300.0f, 560.0f + i * 20}, 128, 1};
-            drcDesc.mroid  = 2;
-            nodeIndex++;
-        }
-    }
-    //*/
     mInit = false;
 }
 
@@ -143,17 +64,7 @@ void RenderCmdWorld::run()
     // }
 
     nodeScene.run();
-
-    // for (auto i = 0; i < cmdsTotal; i++)
-    // {
-    //     nodeScene.cmdBatchNodes[i].update();
-    // }
-    // cmdsTotal = static_cast<uint32_t>(nodeScene.cmdNodes.size());
-    // for (auto i = 0; i < cmdsTotal; i++)
-    // {
-    //     nodeScene.cmdNodes[i].update();
-    // }
-
+    
     auto& view      = canvas.view;
     auto& camDesc   = bufBuilder.camDesc;
     camDesc.projMat = view.projMat;
