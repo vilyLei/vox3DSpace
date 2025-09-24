ba6e04003aa7b105d782af8a669a6badf766ab07
vilyLei <vily313@126.com>
2025-09-24 23:40:47 +0800

    chore: adjust sdf shape drawing test codes.

--- voxol/src/Voass/Render/SDFShaderCode.cpp
+++ voxol/src/Voass/Render/SDFShaderCode.cpp
@@ -1,4 +1,5 @@
 #include "SDFShaderCode.h"
+#include <cstdio>
 namespace Voass::Render {
 namespace Shader {
 
@@ -42,11 +43,74 @@ void              main()
     fragColor = vec4(u_color.rgb * alpha, alpha);
 }
 )";
+
+
+const char* sdfFragSourceHead = R"(#version 330 core
+    precision     mediump float;
+uniform vec4      u_color;
+in vec2           v_uv;
+out vec4          fragColor;
+
+float aa(float d) {
+    float factor = fwidth(d);
+    return clamp(0.5 - d / factor, 0.0, 1.0);
+}
+)";
+
+const char* sdfFragSourceFuncs = R"(
+    
+float circleSdf(float radius, vec2 center, vec2 xy) {
+    return length(xy - center) - radius;
+}
+
+)";
+
+
+const char* sdfCircleFragSource = R"(
+  
+void main()
+{
+    vec2 center = vec2(0.5, 0.5);
+    float d = circleSdf(0.5, center, v_uv);
+    
+    float alpha = aa(d) * u_color.a;
+
+    fragColor = vec4(u_color.rgb * alpha, alpha);
+}
+)";
+
 const char* getSdfVertShdCode() {
     return sdfVertSource;
 }
-const char* getSdfFragShdCode(SDFShapeType type) {
-    return sdfFragSource;
+std::string source{};
+
+const char* getSdfFragShdCode(SDFShapeType type)
+{
+    std::string head = sdfFragSourceHead;
+    std::string funcs = sdfFragSourceFuncs;
+    source = head + funcs;
+    switch (type)
+    {
+        case Voass::Render::Shader::SDFShapeType::Circle:
+            break;
+        case Voass::Render::Shader::SDFShapeType::MultiCircles:
+            break;
+        case Voass::Render::Shader::SDFShapeType::Ring:
+            break;
+        case Voass::Render::Shader::SDFShapeType::Rect:
+            break;
+        case Voass::Render::Shader::SDFShapeType::RoundedRect:
+            break;
+        case Voass::Render::Shader::SDFShapeType::Triangle:
+            break;
+        case Voass::Render::Shader::SDFShapeType::Star:
+            break;
+        default:
+            break;
+    }
+    source += sdfCircleFragSource;
+    //printf("source: \n%s\n", source.data());
+    return source.data();
 }
 }
 }
