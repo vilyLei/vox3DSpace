ba6b6172d76ea72d99fc6569bd8cd95a6e0d9a43
vilyLei <vily313@126.com>
2025-10-13 22:16:11 +0800

    chore: adjust shader code functions.

--- voxol/src/Voass/Render/SDFShaderCode.cpp
+++ voxol/src/Voass/Render/SDFShaderCode.cpp
@@ -62,39 +62,6 @@ void              main()
 }
 )";
 
-
-//const char* sdfFragSourceHead = R"(#version 330 core
-//    precision     mediump float;
-//uniform vec4      u_color;
-//in vec2           v_uv;
-//out vec4          fragColor;
-//
-//const float PI = 3.14159265359;
-//const float factor = 0.5 / 255.0;
-//const float c = 43758.5453;
-//const vec2 uvFactor = vec2( 12.9898 ,78.233 );
-//float randUV( vec2 uv ) {
-//    float dt = dot( uv.xy, uvFactor );
-//    float sn = mod( dt, PI );
-//    return fract(sin(sn) * c);
-//}
-//const vec4 dither_fector = vec4( factor, -factor, factor, factor);
-//vec4 dithering( vec4 color, vec2 puv ) {
-//    float grid_position = randUV( puv );
-//    
-//    return color + mix( 2.0 * dither_fector, -2.0 * dither_fector, grid_position );
-//    //color = pow(color, vec4(2.2));
-//    //color = color + mix( 2.0 * dither_fector, -2.0 * dither_fector, grid_position );
-//    //return pow(color, vec4(1.0/2.2));
-//}
-//float aa(float d) {
-//    float factor = fwidth(d);
-//    //return clamp(0.5 - d / factor, 0.0, 1.0);
-//    return smoothstep(0.0, factor, -d);
-//}
-//)";
-
-
 const char* sdfFragSourceClipDef = R"(
 
 #define SDF_COLOR_CLIP 1
@@ -215,7 +182,8 @@ const char* sdfRoundedRectFragSource = R"(
 void main()
 {
     vec2 center = vec2(0.5, 0.5);
-    float d = buildRoundRect(v_uv, center, vec2(0.5, 0.5), vec4(0.2, 0.1, 0.4, 0.1));
+    //float d = buildRoundRect(v_uv, center, vec2(0.5, 0.5), vec4(0.2, 0.1, 0.4, 0.1));
+    float d = sdfRoundRect(v_uv - center, vec2(0.5, 0.5), vec4(0.2, 0.1, 0.4, 0.1));
     fragColor = buildFragColor(u_color, d);
 }
 )";
