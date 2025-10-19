b371112ae4e931c5ff68649a4e089651b05d7606
vilyLei <vily313@126.com>
2025-10-19 23:10:47 +0800

    chore: adjust some codes.

--- voxol/src/Voxol/Render/EntityHistoryManager.cpp
+++ voxol/src/Voxol/Render/EntityHistoryManager.cpp
@@ -13,12 +13,16 @@ void EntityHistoryManager::initalize()
 
 HistoryItemData EntityHistoryManager::popItem() {
 
-    if(mList.empty()) {
-        HistoryItemData item{};
-        item.id = -1;
-        return item;
+    if (mList.empty())
+    {
+        return HistoryItemData{.id = -1};
     }
-    auto&& itemData = mList.back();
+    // 下面这句auto会导致debug模式下ide看到的itemData值是错的
+    //auto itemData = mList.back();
+    // ，但是下面这样指明类型就是对的了
+    //HistoryItemData itemData = std::move(mList.back());
+
+    auto&&          itemData = mList.back();
     mList.pop_back();
     return itemData;
 }
