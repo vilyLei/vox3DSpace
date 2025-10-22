5c0b8cc68128d9d26dd4681e388a5523537d1b4b
vilyLei <vily313@126.com>
2025-10-22 21:36:19 +0800

    chore: adjust some codes.

--- voxol/src/Voxol/System/UIOperationLayer.cpp
+++ voxol/src/Voxol/System/UIOperationLayer.cpp
@@ -1,6 +1,73 @@
 #include "UIOperationLayer.h"
 namespace Voxol::System
 {
+namespace Mouse
+{
+
+void MouseCtroller::selectSingle(const System::Mouse::MouseEvent& evt, const Math::Vec2& offset)
+{
+    auto bvh = targetSys->bvh;
+    if (evt.isMoving() || evt.isBegin())
+    {
+        qeIds.clear();
+        bvh->queryPoint(evt.globalPos, qeIds);
+    }
+    auto etStorage = targetSys->entityStorage->comp;
+
+    int32_t topId = qeIds.empty() ? -1 : qeIds.back();
+
+    if (evt.isBegin())
+    {
+        dragging = false;
+    }
+
+    if (topId >= 0 && evt.isBegin())
+    {
+        etId          = topId;
+        originEtPos   = etStorage->getEntityXYAt(etId);
+        unitTransform = etStorage->getEntityTransformAt(etId);
+        return;
+    }
+
+    if (etId >= 0 && evt.isDragging())
+    {
+        auto id = etId;
+        auto pv = originEtPos;
+
+        pv += offset;
+        etStorage->setEntityXYAt(pv, id);
+        auto b0 = bvh->getBoundsAt(id);
+        auto b1 = b0;
+
+        // move out
+        tileSys->addDirtyBounds(b0, 0);
+        b1.moveTo(pv.x, pv.y);
+        // move in
+        tileSys->addDirtyBounds(b1, 1);
+
+        bvh->updateItemBoundsByObjectId(id, b1);
+        bvh->updateDirty();
+        dragging = true;
+        return;
+    }
+    if (evt.isEnd())
+    {
+        if (etId >= 0 && dragging)
+        {
+            dragging = false;
+            etStorage->historyManager->pushItem({unitTransform, etId});
+        }
+        etId = -1;
+    }
+}
+
+void MouseCtroller::upateLeftMouseParam(const Render::Draw::DrawContext& rctx, const System::Mouse::MouseInputParam& param)
+{
+    handler.upateMouseLeftBtnParam(rctx, param, [&, this](const System::Mouse::MouseEvent& evt, const Math::Vec2& offset) {
+        selectSingle(evt, offset);
+    });
+}
+}
 void UIOperationLayer::initialize()
 {
     mouseCtrl.tileSys   = tileSys;
--- voxol/src/Voxol/System/UIOperationLayer.h
+++ voxol/src/Voxol/System/UIOperationLayer.h
@@ -36,68 +36,17 @@ struct MouseCtroller
 
     bool dragging = false;
 
-    void selectSingle(const System::Mouse::MouseEvent& evt, const Math::Vec2& offset)
+    bool isBoundsSelection() const
     {
-        auto bvh = targetSys->bvh;
-        if (evt.isMoving() || evt.isBegin())
-        {
-            qeIds.clear();
-            bvh->queryPoint(evt.globalPos, qeIds);
-        }
-        auto etStorage = targetSys->entityStorage->comp;
-
-        int32_t topId = qeIds.empty() ? -1 : qeIds.back();
-
-        if (evt.isBegin()) {
-            dragging = false;
-        }
-
-        if (topId >= 0 && evt.isBegin())
-        {
-            etId          = topId;
-            originEtPos   = etStorage->getEntityXYAt(etId);
-            unitTransform = etStorage->getEntityTransformAt(etId);
-            return;
-        }
-
-        if (etId >= 0 && evt.isDragging())
-        {
-            auto id = etId;
-            auto pv = originEtPos;
-
-            pv += offset;
-            etStorage->setEntityXYAt(pv, id);
-            auto b0 = bvh->getBoundsAt(id);
-            auto b1 = b0;
-
-            // move out
-            tileSys->addDirtyBounds(b0, 0);
-            b1.moveTo(pv.x, pv.y);
-            // move in
-            tileSys->addDirtyBounds(b1, 1);
-
-            bvh->updateItemBoundsByObjectId(id, b1);
-            bvh->updateDirty();
-            dragging = true;
-            return;
-        }
-        if (evt.isEnd())
-        {
-            if (etId >= 0 && dragging)
-            {
-                dragging = false;
-                etStorage->historyManager->pushItem({unitTransform, etId});
-            }
-            etId = -1;
-        }
+        return selectType == SelectType::Bounds;
     }
-
-    void upateLeftMouseParam(const Render::Draw::DrawContext& rctx, const System::Mouse::MouseInputParam& param)
+    bool isSingleSelection() const
     {
-        handler.upateMouseLeftBtnParam(rctx, param, [&, this](const System::Mouse::MouseEvent& evt, const Math::Vec2& offset) {
-            selectSingle(evt, offset);
-        });
+        return selectType == SelectType::Single;
     }
+    void selectSingle(const System::Mouse::MouseEvent& evt, const Math::Vec2& offset);
+
+    void upateLeftMouseParam(const Render::Draw::DrawContext& rctx, const System::Mouse::MouseInputParam& param);
 };
 } // namespace Mouse
 
