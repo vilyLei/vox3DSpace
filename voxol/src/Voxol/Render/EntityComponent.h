2547b885f70c86006b06a8663569e34c44e69ced
vilyLei <vily313@126.com>
2025-11-01 18:24:39 +0800

    chore: adjust some codes.

--- voxol/src/Voxol/Render/EntityComponent.h
+++ voxol/src/Voxol/Render/EntityComponent.h
@@ -138,6 +138,54 @@ struct UnitInstanceIdManager
     uint32_t allocate();
     void     release(uint32_t id);
 };
+
+struct InsNodeSlot
+{
+    uint32_t    id;
+    Math::Mat33 mat;
+    bool        active;
+};
+
+struct FlatInsStorage
+{
+    std::vector<InsNodeSlot>               nodes;
+    std::unordered_map<uint32_t, uint32_t> idToIndex;
+    std::vector<uint32_t>                  freeList;
+
+    InsNodeSlot& emplace(uint32_t id, const Math::Mat33& mat)
+    {
+        if (!freeList.empty())
+        {
+            auto idx = freeList.back();
+            freeList.pop_back();
+            nodes[idx]    = {id, mat, true};
+            idToIndex[id] = idx;
+            return nodes[idx];
+        }
+        uint32_t idx = nodes.size();
+        nodes.push_back({id, mat, true});
+        idToIndex[id] = idx;
+        return nodes.back();
+    }
+
+    void erase(uint32_t id)
+    {
+        auto it = idToIndex.find(id);
+        if (it != idToIndex.end())
+        {
+            uint32_t idx      = it->second;
+            nodes[idx].active = false;
+            freeList.push_back(idx);
+            idToIndex.erase(it);
+        }
+    }
+
+    InsNodeSlot* find(uint32_t id)
+    {
+        auto it = idToIndex.find(id);
+        return it != idToIndex.end() ? &nodes[it->second] : nullptr;
+    }
+};
 } // namespace Component
 
 } // namespace Voxol::Render
