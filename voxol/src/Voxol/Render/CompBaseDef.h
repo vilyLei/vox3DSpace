f5f28832ac01e2e207b9779e542b71133a48bd56
vilyLei <vily313@126.com>
2025-11-02 10:48:49 +0800

    chore: adjust some codes.

--- voxol/src/Voxol/Render/CompBaseDef.h
+++ voxol/src/Voxol/Render/CompBaseDef.h
@@ -18,20 +18,12 @@ namespace Base
 // 28-bit invalid ID
 constexpr uint32_t INVALID_ID = 0xfffffff;
 
-constexpr inline bool isValidID(uint32_t id) noexcept
-{
-    return id != INVALID_ID;
-}
-constexpr inline bool isInvalidID(uint32_t id) noexcept
-{
-    return id == INVALID_ID;
-}
-
 struct EntityId
 {
     uint32_t value = INVALID_ID;
 
-    constexpr bool valid() const noexcept { return value != INVALID_ID; }
+    constexpr bool isValid() const noexcept { return value < INVALID_ID; }
+    constexpr bool isInvalid() const noexcept { return value >= INVALID_ID; }
 
     constexpr bool operator==(const EntityId& other) const noexcept
     {
@@ -119,6 +111,10 @@ struct KeyUint64
     static constexpr uint64_t FlagMask    = 0xFFFFull << 56;  // 16-bit << 48
     static constexpr uint64_t CompareMask = (1ull << 56) - 1; // lower 48 bits
 
+    static constexpr KeyUint64 makeDefault()
+    {
+        return KeyUint64{0};
+    }
     static constexpr KeyUint64 make(uint32_t protoId, uint32_t iid, uint16_t flags = 0)
     {
         return KeyUint64{(uint64_t(flags) << 56) | (uint64_t(iid) << 28) | uint64_t(protoId)};
@@ -126,8 +122,21 @@ struct KeyUint64
 
     constexpr uint16_t flags() const noexcept { return value >> 56; }
     constexpr uint32_t protoNodeId() const noexcept { return value & ProtoMask; }
+    constexpr bool     isProtoNodeIdValid() const noexcept { return (value & ProtoMask) < INVALID_ID; }
+    constexpr bool     isProtoNodeIdInvalid() const noexcept { return (value & ProtoMask) >= INVALID_ID; }
+
     constexpr uint32_t iid() const noexcept { return (value >> 28) & ProtoMask; }
+    constexpr bool     isIIDValid() const noexcept
+    {
+        return ((value >> 28) & ProtoMask) < INVALID_ID;
+    }
+    constexpr bool isIIDInvalid() const noexcept
+    {
+        return ((value >> 28) & ProtoMask) >= INVALID_ID;
+    }
     constexpr uint32_t id() const noexcept { return value & CompareMask; }
+    constexpr bool     isIDValid() const noexcept { return isProtoNodeIdValid() && isIIDValid(); }
+    constexpr bool     isIDInvalid() const noexcept { return isProtoNodeIdInvalid() || isIIDInvalid(); }
 
     constexpr bool operator==(const KeyUint64& other) const noexcept
     {
@@ -157,9 +166,28 @@ struct KeyUint64Equal
 {
     bool operator()(const KeyUint64& a, const KeyUint64& b) const noexcept
     {
-        return (KeyUint64::CompareMask & a.value) == (KeyUint64::CompareMask & b.value);
+        return (a.value & KeyUint64::CompareMask) == (b.value & KeyUint64::CompareMask);
     }
 };
+
+constexpr inline bool isValidID(uint32_t id) noexcept
+{
+    return id < INVALID_ID;
+}
+constexpr inline bool isInvalidID(uint32_t id) noexcept
+{
+    return id >= INVALID_ID;
+}
+
+constexpr inline bool isValidID(const KeyUint64& id) noexcept
+{
+    return id.isIDValid();
+}
+constexpr inline bool isInvalidID(const KeyUint64& id) noexcept
+{
+    return id.isIDInvalid();
+}
+
 } // namespace Base
 
 } // namespace Voxol::Render
