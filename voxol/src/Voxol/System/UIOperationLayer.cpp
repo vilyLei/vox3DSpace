34b6f067edbf22aaa80bc875c0b8675db501b1cb
vilyLei <vily313@126.com>
2025-11-05 23:52:37 +0800

    chore: adjust some codes.

--- voxol/src/Voxol/System/UIOperationLayer.cpp
+++ voxol/src/Voxol/System/UIOperationLayer.cpp
@@ -23,7 +23,11 @@ void MouseController::selectWithSingle(const System::Mouse::MouseEvent& evt, con
     etStorage->checkIds(qeIds);
 
     auto topId = qeIds.empty() ? Render::ID::INVALID_KEY : qeIds.back();
-
+    if (topId.flags() > 0)
+    {
+        etId = Render::ID::INVALID_KEY;
+        return;
+    }
     if (evt.isBegin())
     {
         dragging = false;
