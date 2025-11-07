f762b7abb382b3d194389644eec7ac0cfcb094bc
vilyLei <vily313@126.com>
2025-11-07 21:41:27 +0800

    chore: adjust some codes.

--- voxol/src/Voxol/Render/EntitySceneSystem.cpp
+++ voxol/src/Voxol/Render/EntitySceneSystem.cpp
@@ -64,8 +64,9 @@ void EntitySceneSystem::initalize(const std::string& configFileName)
         auto&& vb = storage->getEntityGlobalBoundsAt(et.id);
         bvh->addItem(ID::KeyUint64::make(et.id), vb);
         auto& wmats = storage->entityInsGlobalMat33Map;
-        if (instanceStorage.contains(et.id))
+        if (!instanceStorage.contains(et.id))
             return;
+
         auto&& insMap = instanceStorage[et.id];
         for (auto& item : insMap.map)
         {
