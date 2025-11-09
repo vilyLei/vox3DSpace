d754b5257307de1124b8c3c55da8ffd4203d0b28
vilyLei <vily313@126.com>
2025-11-09 17:43:22 +0800

    chore: adjust some codes.

--- voxol/src/Voxol/Tile/TileScene.cpp
+++ voxol/src/Voxol/Tile/TileScene.cpp
@@ -75,8 +75,8 @@ void TileScene::buildGridContent(Grid::Unit& unit, const Render::Draw::DrawConte
     fboCtx.clearParam = clearParam;
     fboCtx.texUnits   = {{0, gridSize, gridSize, drawUnit.getTextureAt(0)}};
 
-    std::string debugEnterInfo = std::format("Tile AAA RC({}, {})", static_cast<int>(unit.rc.r), static_cast<int>(unit.rc.c));
-    std::string debugExitInfo  = std::format("Tile AAA RC({}, {})\n", static_cast<int>(unit.rc.r), static_cast<int>(unit.rc.c));
+    std::string debugEnterInfo = std::format("Tile AAA RC({}, {}) Begin", static_cast<int>(unit.rc.r), static_cast<int>(unit.rc.c));
+    std::string debugExitInfo  = std::format("Tile AAA RC({}, {}) End\n", static_cast<int>(unit.rc.r), static_cast<int>(unit.rc.c));
 
     auto&& nodeGuard      = ctx.makeFBOGraphNodeGuard(fboCtx, debugEnterInfo, debugExitInfo);
 
