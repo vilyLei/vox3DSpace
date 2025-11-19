8ddd1b979d53387e4754cc10250ead77aafc9197
vilyLei <vily313@126.com>
2025-11-19 22:13:49 +0800

    fix: add scene description functions.

---
+++ voxol/src/Voxol/Scene/DescriptionParser.cpp
@@ -0,0 +1,91 @@
+#include "DescriptionParser.h"
+#include <fstream>
+#include <filesystem>
+
+namespace Voxol::Scene
+{
+namespace Desc
+{
+void SceneNode::parse(const JsonType& jsonNode)
+{
+    if (jsonNode.contains("type"))
+    {
+        type = jsonNode["type"];
+    }
+    if (jsonNode.contains("name"))
+    {
+        name = jsonNode["name"];
+    }
+    if (jsonNode.contains("children") && jsonNode["children"].is_array())
+    {
+        auto&& elements = jsonNode["children"];
+        hasChild        = !elements.empty();
+    }
+}
+void SceneNode::print()
+{
+
+    std::string hasChildStr = ", hasChild=" + (hasChild ? std::string("true") : std::string("false"));
+    std::string info        = "Node(name=" + name + ",type=" + type + ", childrenTotal=" + std::to_string(childrenTotal) + hasChildStr + ")";
+    printf("%s\n", info.c_str());
+}
+
+void FileParser::initFromFile(const std::string& fileName)
+{
+    auto           pngPath = std::filesystem::path(SRC_DIR) / "assets/";
+    auto           pathStr = pngPath.string() + fileName;
+    std::ifstream  fs(pathStr);
+    nlohmann::json jsonObj;
+    fs >> jsonObj;
+
+    parseHeriNodes(jsonObj["scene"]);
+}
+void FileParser::parseHeriNodes(const JsonType& jsonNode)
+{
+    if (jsonNode.contains("objects") && jsonNode["objects"].is_array())
+    {
+        auto&& elements = jsonNode["objects"];
+
+        auto sizeValue = elements.size();
+
+        if (elements.empty())
+            return;
+
+        rootNode.hasParent = false;
+        for (auto& item : elements)
+        {
+            parseSceneNode(rootNode, item);
+            rootNode.print();
+        }
+    }
+}
+void FileParser::parseSceneNode(SceneNode& parentNode, const JsonType& jsonNode)
+{
+
+    parentNode.parse(jsonNode);
+
+    if (!parentNode.hasChild)
+        return;
+
+    if (jsonNode.contains("children") && jsonNode["children"].is_array())
+    {
+        auto&& elements = jsonNode["children"];
+        if (elements.empty())
+            return;
+        for (auto& item : elements)
+        {
+            SceneNode node;
+            parseSceneNode(node, item);
+            node.print();
+            parentNode.children.emplace_back(std::move(node));
+        }
+        parentNode.childrenTotal = static_cast<int>(parentNode.children.size());
+    }
+}
+} // namespace Desc
+
+void DescriptionParser::initialize()
+{
+}
+
+} // namespace Voxol::Scene
---
+++ voxol/src/Voxol/Scene/DescriptionParser.h
@@ -0,0 +1,59 @@
+#ifndef VOXOL_SCENE_DESCRIPTION_PARSER_H
+#define VOXOL_SCENE_DESCRIPTION_PARSER_H
+
+#include "../Base/BaseDefine.h"
+#include "EntityComponent.h"
+#include "../Math/Vec2.h"
+#include <nlohmann/json.hpp>
+#include <string>
+
+namespace Voxol::Scene
+{
+namespace Desc
+{
+using JsonType = nlohmann::json;
+
+struct SceneNode
+{
+    std::string            type;
+    std::string            name;
+    int                    index = 0;
+    std::vector<SceneNode> children;
+
+    int childrenTotal = 0;
+
+    bool hasChild  = false;
+    bool hasParent = true;
+
+    void parse(const JsonType& jsonNode);
+    void print();
+};
+
+class FileParser
+{
+public:
+    FileParser()  = default;
+    ~FileParser() = default;
+
+public:
+    SceneNode rootNode;
+
+public:
+    void initFromFile(const std::string& fileName);
+    void parseHeriNodes(const JsonType& jsonNode);
+    void parseSceneNode(SceneNode& parentNode, const JsonType& jsonNode);
+};
+} // namespace Desc
+
+class DescriptionParser
+{
+public:
+    DescriptionParser()  = default;
+    ~DescriptionParser() = default;
+
+public:
+    void initialize();
+};
+
+} // namespace Voxol::Scene
+#endif
