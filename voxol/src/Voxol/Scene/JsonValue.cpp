67822b6957ee8911cb1bce344d100ad84779cc01
vilyLei <vily313@126.com>
2025-11-21 23:04:45 +0800

    chore: adjust some codes.

---
+++ voxol/src/Voxol/Scene/JsonValue.cpp
@@ -0,0 +1,105 @@
+#include "JsonValue.h"
+
+namespace Voxol::Scene
+{
+namespace Desc
+{
+
+void JsonValue::parse(const JsonType& j)
+{
+    if (j.is_null())
+    {
+        type  = Type::Null;
+        value = std::monostate{};
+    }
+    else if (j.is_boolean())
+    {
+        type  = Type::Bool;
+        value = static_cast<bool>(j);
+    }
+    else if (j.is_number_float())
+    {
+        type  = Type::Float;
+        value = static_cast<float>(j);
+    }
+    else if (j.is_number_integer())
+    {
+        type  = Type::Int;
+        value = static_cast<int>(j);
+    }
+    else if (j.is_number_unsigned())
+    {
+        type  = Type::Uint32;
+        value = static_cast<uint32_t>(j);
+    }
+    else if (j.is_string())
+    {
+        type  = Type::String;
+        value = j.get<std::string>();
+    }
+    else if (j.is_array())
+    {
+        // vec2/vec3/vec4 auto-detect
+        if (j.size() == 2 && j[0].is_number() && j[1].is_number())
+        {
+            type  = Type::Vec2;
+            value = Math::Vec2{(float)j[0], (float)j[1]};
+            return;
+        }
+        if (j.size() == 3)
+        {
+            bool allNum = true;
+            for (auto& e : j)
+                if (!e.is_number()) allNum = false;
+            if (allNum)
+            {
+                type  = Type::Vec3;
+                value = Math::Vec3{(float)j[0], (float)j[1], (float)j[2]};
+                return;
+            }
+        }
+        if (j.size() == 4)
+        {
+            bool allNum = true;
+            for (auto& e : j)
+                if (!e.is_number()) allNum = false;
+            if (allNum)
+            {
+                type  = Type::Vec4;
+                value = Math::Vec4{
+                    (float)j[0], (float)j[1],
+                    (float)j[2], (float)j[3]};
+                return;
+            }
+        }
+
+        // general array
+        type = Type::Array;
+        {
+            ArrayType arr;
+            arr.reserve(j.size());
+            for (auto& item : j)
+            {
+                JsonValue val;
+                val.parse(item);
+                arr.push_back(std::move(val));
+            }
+            value = std::move(arr);
+        }
+    }
+    else if (j.is_object())
+    {
+        type = Type::Object;
+        ObjectType obj;
+        for (auto it = j.begin(); it != j.end(); ++it)
+        {
+            JsonValue val;
+            val.parse(it.value());
+            obj[it.key()] = std::move(val);
+        }
+        value = std::move(obj);
+    }
+}
+}
+} // namespace Voxol::Scene
+"
---
+++ voxol/src/Voxol/Scene/JsonValue.h
@@ -0,0 +1,92 @@
+#ifndef VOXOL_SCENE_DESC_JSON_VALUE_H
+#define VOXOL_SCENE_DESC_JSON_VALUE_H
+
+#include <variant>
+#include <nlohmann/json.hpp>
+#include "../Math/Vec2.h"
+#include "../Math/Vec3.h"
+#include "../Math/Vec4.h"
+
+namespace Voxol::Scene
+{
+namespace Desc
+{
+using JsonType = nlohmann::json;
+
+struct JsonValue
+{
+    enum class Type : uint8_t
+    {
+        Null,
+        Bool,
+        Int,
+        Uint32,
+        Float,
+        String,
+        Vec2,
+        Vec3,
+        Vec4,
+        Array,
+        Object
+    };
+
+    using ArrayType  = std::vector<JsonValue>;
+    using ObjectType = std::unordered_map<std::string, JsonValue>;
+
+    using Variant = std::variant<
+        std::monostate, // Null
+        bool,
+        int,
+        uint32_t,
+        float,
+        std::string,
+        Math::Vec2,
+        Math::Vec3,
+        Math::Vec4,
+        ArrayType,
+        ObjectType>;
+
+    Type    type = Type::Null;
+    Variant value;
+
+    JsonValue() = default;
+
+    JsonValue(float v) :
+        type(Type::Float), value(v) {}
+    JsonValue(int v) :
+        type(Type::Int), value(v) {}
+    JsonValue(uint32_t v) :
+        type(Type::Uint32), value(v) {}
+    JsonValue(bool v) :
+        type(Type::Bool), value(v) {}
+    JsonValue(const std::string& s) :
+        type(Type::String), value(s) {}
+    JsonValue(const char* s) :
+        type(Type::String), value(std::string(s)) {}
+
+    JsonValue(const Math::Vec2& v2) :
+        type(Type::Vec2), value(v2) {}
+    JsonValue(const Math::Vec3& v3) :
+        type(Type::Vec3), value(v3) {}
+    JsonValue(const Math::Vec4& v4) :
+        type(Type::Vec4), value(v4) {}
+
+    JsonValue(const ArrayType& arr) :
+        type(Type::Array), value(arr) {}
+    JsonValue(const ObjectType& obj) :
+        type(Type::Object), value(obj) {}
+
+    void parse(const JsonType& j);
+
+    template <typename T>
+    bool is() const { return std::holds_alternative<T>(value); }
+
+    template <typename T>
+    T& get() { return std::get<T>(value); }
+
+    template <typename T>
+    const T& get() const { return std::get<T>(value); }
+};
+} // namespace Desc
+} // namespace Voxol::Scene
+#endif
