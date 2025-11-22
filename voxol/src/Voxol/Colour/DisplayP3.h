c38416ff3a8d18d426de54579a135b0af4f2e85e
vilyLei <vily313@126.com>
2025-11-22 08:49:10 +0800

    chore: adjust some codes.

---
+++ voxol/src/Voxol/Colour/DisplayP3.h
@@ -0,0 +1,14 @@
+#ifndef VOXOL_COLOUR_DISPLAY_P3_H
+#define VOXOL_COLOUR_DISPLAY_P3_H
+
+#include "../Base/BaseDefine.h"
+#include <cstdint>
+
+namespace Voxol::Colour
+{
+namespace DisplayP3
+{
+
+}
+}
+#endif
