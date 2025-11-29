78883e5f898a4ba881da84a5ceb45d46fd981411
vilyLei <vily313@126.com>
2025-11-29 11:15:11 +0800

    chore: adjust some codes.

--- voxol/src/Voxol/Scene/DescriptionParser.cpp
+++ voxol/src/Voxol/Scene/DescriptionParser.cpp
@@ -494,34 +494,9 @@ void FileParser::parseNodeDisplayShape(SceneNode& node, const JsonType& jsonNode
     shape.parse(jNode);
     node.transform.scale() = shape.size;
     node.unitModel.id      = node.id;
-    Component::updateUunitModel(node.unitModel, shape.type);
-    /*
-    if (shape.type == "rectangle")
-    {
-        node.unitModel.drawUnitId = 0;
-        node.unitModel.type       = Component::UnitModelType::Mesh;
-    }
-    else if (shape.type == "round-rectangle")
-    {
-        node.unitModel.drawUnitId = 5;
-        node.unitModel.type       = Component::UnitModelType::Mesh;
-    }
-    else if (shape.type == "circle")
-    {
-        node.unitModel.drawUnitId = 1;
-        node.unitModel.type       = Component::UnitModelType::Mesh;
-    }
-    else if (shape.type == "text")
-    {
-        node.unitModel.drawUnitId = 8;
-        node.unitModel.type       = Component::UnitModelType::Text;
 
-        auto&                    textDesc = shape.jsonValue.get<Component::UnitTextDesc>();
-        Component::UnitTextModel textModel;
-        textModel.text             = textDesc;
-        textModelMap[textModel.id] = textModel;
-    }
-    //*/
+    Component::updateUunitModel(node.unitModel, shape.type);
+
     if (node.unitModel.isText())
     {
         auto&                    textDesc = shape.jsonValue.get<Component::UnitTextDesc>();
