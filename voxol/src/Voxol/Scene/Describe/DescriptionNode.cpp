e4d574032a65c7462c5baa39a6c73c76a003d7b3
vilyLei <vily313@126.com>
2025-11-29 13:31:53 +0800

    chore: adjust some codes.

---
+++ voxol/src/Voxol/Scene/Describe/DescriptionNode.cpp
@@ -0,0 +1,7 @@
+
+#include "DescriptionNode.h"
+
+namespace Voxol::Scene::Describe
+{
+
+} // namespace Voxol::Scene::Describe
---
+++ voxol/src/Voxol/Scene/Describe/DescriptionNode.h
@@ -0,0 +1,34 @@
+#ifndef VOXOL_SCENE_DESCRITION_NODE_H
+#define VOXOL_SCENE_DESCRITION_NODE_H
+
+#include "../Base/BaseDefine.h"
+#include "EntityComponent.h"
+#include "Intent/IntentComponent.h"
+#include "JsonValue.h"
+#include "../Math/Vec2.h"
+#include <nlohmann/json.hpp>
+#include <string>
+#include <functional>
+
+namespace Voxol::Scene::Describe
+{
+using JsonType = nlohmann::json;
+
+struct HierarchyNode
+{
+    uint32_t                 id = Base::ID::INVALID_ID;
+    std::string              name;
+    Component::UnitHierarchy hieraychy;
+    void                     print()
+    {
+        std::string info = ", parent=" + std::to_string(hieraychy.parent);
+        info += ", firstChild=" + std::to_string(hieraychy.firstChild);
+        info += ", next=" + std::to_string(hieraychy.next);
+        info += ", id=" + std::to_string(id);
+        info = "HierarchyNode(name=" + name + info + ")";
+        printf("%s\n", info.c_str());
+    }
+};
+
+} // namespace Voxol::Scene::Layout
+#endif
