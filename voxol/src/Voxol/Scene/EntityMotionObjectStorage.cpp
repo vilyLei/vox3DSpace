9d2bd8f620faa59c4c27d6d952156e3936219b8c
vilyLei <vily313@126.com>
2025-12-07 00:15:33 +0800

    chore: adjust some codes.

--- voxol/src/Voxol/Scene/EntityMotionObjectStorage.cpp
+++ voxol/src/Voxol/Scene/EntityMotionObjectStorage.cpp
@@ -73,7 +73,7 @@ void EntityMotionObjectStorage::update()
         for (auto& obj : objs) {
             auto&& pos = obj->entityView->globalPos();
             auto   dv  = pos - currPos;
-            if (dv.lengthSquared() < 500)
+            if (dv.lengthSquared() < 600)
             {
                 //printf("........... hit ....fdfd\n");
                 foodStorage->setObject(obj->entityView->etProtoId(), nullptr);
